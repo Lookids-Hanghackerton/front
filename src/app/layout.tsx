cc99db9ecb7a4626b3ff1d225f3ef3c48b456571
csyun <changsoonyun@naver.com>
2023-09-20 20:46:37 +0900

    Feat : 전체 레이아웃에서 Header 삭제 후 각 라우트 별 Header 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "../styles/input.css";
 import type { Metadata } from "next";
 import { ReactNode } from "react";
-import Header from "@/layout/Header";
 import Navbar from "@/layout/Navbar";
 import ReactQueryProvider from "@/utils/react-query/ReactQueryProvider";
 
@@ -17,10 +16,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         <ReactQueryProvider>
           <div className={"flex flex-col w-full max-h-screen h-screen bg-gray-50"}>
             <div className={"flex flex-col flex-1 max-w-[767px] w-full m-auto shadow overflow-auto"}>
-              <div className={"flex-1 bg-white overflow-auto no-scroll "}>
-                <Header />
-                {children}
-              </div>
+              <div className={"flex-1 bg-white overflow-auto no-scroll "}>{children}</div>
               <Navbar />
             </div>
           </div>
