3c62653a82c926bb8184ff1bdc726018c390bd12
csyun <changsoonyun@naver.com>
2023-09-20 20:54:42 +0900

    Feat : DefaultHeader 및 ArrowHeader 추가

---
+++ src/layout/header/ArrowHeader.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { BsArrowLeft } from "react-icons/bs";
+import { ReactNode } from "react";
+import { useRouter } from "next/navigation";
+
+interface ArrowHeaderProps {
+  center?: ReactNode;
+  right?: ReactNode;
+}
+
+const ArrowHeader = ({ center, right }: ArrowHeaderProps) => {
+  const router = useRouter();
+  return (
+    <header className={"flex items-center p-6"}>
+      <BsArrowLeft className={"cursor-pointer"} size={28} onClick={() => router.back()} />
+      <div className={"flex-1"}>{center}</div>
+      <div className={"font-semibold text-lg cursor-pointer"}>{right}</div>
+    </header>
+  );
+};
+
+export default ArrowHeader;
