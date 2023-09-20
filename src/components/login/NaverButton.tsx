a1f5be126fa39bc2f1cfa564070bfd0824209897
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-09-20 23:16:27 +0900

    Feat : 소셜 로그인 버튼 리팩토링

--- src/components/login/NaverButton.tsx
+++ src/components/login/NaverButton.tsx
@@ -1,20 +1,19 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
+import SocialLoginButton from "./SocialLoginButton";
 
 const NaverButton = () => {
   const clientId = process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
   const redirectUri = process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI;
   const state = "STATE_STRING";
-  const callbackUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&state=${state}&redirect_uri=${redirectUri}`;
 
-  return (
-    <div id="naver_id_login" className="relative w-full h-full">
-      <Link href={callbackUrl} target="_blank" rel="noopener noreferrer">
-        <Image src="/images/buttons/naver_btnG_complete.png" alt="네이버 로그인" layout="fill" objectFit="contain" />
-      </Link>
-    </div>
-  );
+  const NAVER_CONFIG = {
+    id: "naver_id_login",
+    callbackUrl: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&state=${state}&redirect_uri=${redirectUri}`,
+    imageUrl: "/images/buttons/naver_btnG_complete.png",
+    alt: "네이버 로그인",
+  };
+
+  return <SocialLoginButton {...NAVER_CONFIG} />;
 };
 
 export default NaverButton;
