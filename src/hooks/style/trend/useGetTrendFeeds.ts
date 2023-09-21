1000c26cbc55c1f052cc7bc12913c30b6c3249db
csyun <changsoonyun@naver.com>
2023-09-21 20:00:44 +0900

    Feat : Feed api prefetch 적용

--- src/hooks/style/trend/useGetTrendFeeds.ts
+++ src/hooks/style/trend/useGetTrendFeeds.ts
@@ -1,16 +1,16 @@
 import { api } from "@utils/axios/api";
 import { useQuery } from "@tanstack/react-query";
 import { FilterItem } from "@components/style/trend/FilteredFeeds";
+import { FeedResponse } from "@/apis/interface/FeedListInterface";
 
-const getTrendFeeds = async (filtered: FilterItem) => {
+export const getTrendFeeds = async (filtered: FilterItem) => {
   const sort = filtered === "최신순" ? "latest" : filtered === "인기순" ? "popular" : undefined;
-  const { data } = await api.get(`style/trend/feeds?sort=${sort}&page=0`);
-
-  return data;
+  const { data: feedList } = await api.get(`style/trend/feeds?sort=${sort}&page=0`);
+  return feedList;
 };
 
-export const useGetTrendFeeds = (filtered: FilterItem) => {
-  const { data } = useQuery([`feeds/${filtered}`], () => getTrendFeeds(filtered));
+export const useGetTrendFeeds = ({ filtered, feedList }: { filtered: FilterItem; feedList: FeedResponse }) => {
+  const { data } = useQuery([`feeds/${filtered}`], () => getTrendFeeds(filtered), { initialData: feedList });
 
   return data;
 };
