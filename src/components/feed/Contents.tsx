c7f1d03d47caae66ef708baf9afc3f7319f2e2ec
csyun <changsoonyun@naver.com>
2023-09-21 14:22:26 +0900

    Feat : Feed detail ui

---
+++ src/components/feed/Contents.tsx
@@ -0,0 +1,17 @@
+const Contents = () => {
+  return (
+    <p className={"text-sm pb-8"}>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consectetur cumque dolorum ea
+      exercitationem fuga harum inventore ipsa minima molestias, necessitatibus, odio officiis possimus quia, rem
+      similique voluptas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam commodi consectetur
+      cumque dolorum ea exercitationem fuga harum inventore ipsa minima molestias, necessitatibus, odio officiis
+      possimus quia, rem similique voluptas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam
+      commodi consectetur cumque dolorum ea exercitationem fuga harum inventore ipsa minima molestias, necessitatibus,
+      odio officiis possimus quia, rem similique voluptas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+      aperiam commodi consectetur cumque dolorum ea exercitationem fuga harum inventore ipsa minima molestias,
+      necessitatibus, odio officiis possimus quia, rem similique voluptas.
+    </p>
+  );
+};
+
+export default Contents;
