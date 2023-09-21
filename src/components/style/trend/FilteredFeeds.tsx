588f9ff92a3534b22a214c10f5829fb889f94499
csyun <changsoonyun@naver.com>
2023-09-21 20:01:52 +0900

    Feat : api에 맞춰 컴포넌트 적용

--- src/components/style/trend/FilteredFeeds.tsx
+++ src/components/style/trend/FilteredFeeds.tsx
@@ -2,25 +2,25 @@
 
 import Filters from "@components/style/trend/Filters";
 import { useState } from "react";
+import { useGetTrendFeeds } from "@hooks/style/trend/useGetTrendFeeds";
+import { FeedResponse } from "@/apis/interface/FeedListInterface";
 import Feeds from "@components/style/trend/Feeds";
 
 export type FilterItem = (typeof FILTER_ITEMS)[number];
 
 export const FILTER_ITEMS = ["인기순", "최신순"];
-const FilteredFeeds = () => {
+const FilteredFeeds = ({ feedList }: { feedList: FeedResponse }) => {
   const [filtered, setFiltered] = useState<FilterItem>("인기순");
   const filterHandler = (target: FilterItem) => {
     setFiltered(target);
   };
 
-  // const data = useGetTrendFeeds(filtered);
-
-  // console.log(data);
+  const data = useGetTrendFeeds({ filtered, feedList });
 
   return (
     <div>
       <Filters filtered={filtered} onClick={filterHandler} />
-      <Feeds />
+      <Feeds feeds={data.data.feedsList} />
     </div>
   );
 };
