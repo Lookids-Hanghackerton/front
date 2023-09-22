8c287d16ca5f57688cd2bce41fe002cf08376b45
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-09-22 23:56:47 +0900

    style : 게시글 헤더 style 수정

--- src/layout/header/Right.tsx
+++ src/layout/header/Right.tsx
@@ -3,17 +3,28 @@ import { RxHamburgerMenu } from "react-icons/rx";
 interface RightProps {
   isText: boolean;
   text?: string;
+  onClick?: () => void;
 }
 
 /**
  *
- * @param isText false => 햄버거 버튼 // true => text prop 필요
- * @param text
- */
 
-const Right = ({ isText, text }: RightProps) => {
+@param isText false => 햄버거 버튼 // true => text prop 필요
+@param text
+@param onClick
+*/
+
+const Right = ({ isText, text, onClick }: RightProps) => {
   return (
-    <div className={"pl-6"}>{isText ? <p className={"font-semibold"}>{text}</p> : <RxHamburgerMenu size={28} />}</div>
+    <div className={"pl-6"}>
+      {isText ? (
+        <p className={"font-semibold cursor-pointer"} onClick={onClick}>
+          {text}
+        </p>
+      ) : (
+        <RxHamburgerMenu size={28} />
+      )}
+    </div>
   );
 };
 
