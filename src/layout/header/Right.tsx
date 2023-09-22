af386eef3c59b1d9f1f6b4b79e9643264f31c179
csyun <changsoonyun@naver.com>
2023-09-23 00:47:19 +0900

    Feat : header center right컴포넌트에 onClick, onChange interface 추가

--- src/layout/header/Right.tsx
+++ src/layout/header/Right.tsx
@@ -3,17 +3,27 @@ import { RxHamburgerMenu } from "react-icons/rx";
 interface RightProps {
   isText: boolean;
   text?: string;
+  onClick?: () => void;
 }
 
 /**
  *
  * @param isText false => 햄버거 버튼 // true => text prop 필요
  * @param text
+ * @param onClick
  */
 
-const Right = ({ isText, text }: RightProps) => {
+const Right = ({ isText, text, onClick }: RightProps) => {
   return (
-    <div className={"pl-6"}>{isText ? <p className={"font-semibold"}>{text}</p> : <RxHamburgerMenu size={28} />}</div>
+    <div className={"pl-6"}>
+      {isText ? (
+        <p className={"font-semibold"} onClick={onClick}>
+          {text}
+        </p>
+      ) : (
+        <RxHamburgerMenu size={28} />
+      )}
+    </div>
   );
 };
 
