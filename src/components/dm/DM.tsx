d66637beca5263fd4047ddbb403965d608c758c2
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-09-24 05:19:22 +0900

    Feat : DM UI 업데이트

--- src/components/dm/DM.tsx
+++ src/components/dm/DM.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, Text } from "@chakra-ui/react";
+import { Button, Card, Input, InputGroup, InputRightAddon, Text } from "@chakra-ui/react";
+import { BiMicrophone } from "react-icons/bi";
 
 const DM = () => {
   const date = new Date();
@@ -18,34 +19,55 @@ const DM = () => {
           {dateString}
         </Text>
 
-        <Card className="w-full flex items-end">
+        <Card className="w-full flex items-end" shadow="none">
           <div className="w-60 h-40 rounded-md bg-slate-400" />
-          <Text align="right" fontSize="sm" color="GrayText">
-            확인함
-          </Text>
         </Card>
 
-        <Card className="w-full flex items-end">
+        <Card className="w-full flex items-end" shadow="none">
           <div className="w-60 h-60 rounded-md bg-slate-400" />
           <Text align="right" fontSize="sm" color="GrayText">
             확인함
           </Text>
         </Card>
 
-        <Card className="w-full flex items-end">
-          <div className="w-60 px-6 py-4 mb-6 rounded-md bg-slate-200">
+        <Card className="w-full flex items-end" shadow="none">
+          <div className="w-60 px-6 py-4 rounded-md bg-slate-200">
             <Text fontSize="sm" color="GrayText">
-              확인함
+              안녕하세요
             </Text>
           </div>
         </Card>
 
-        <Card className="w-full flex items-start">
+        <Card className="w-full flex items-end" shadow="none">
+          <div className="w-60 px-6 py-4 mb-6 rounded-md bg-slate-200">
+            <Text fontSize="sm" color="GrayText">
+              텍스트입니다.
+            </Text>
+          </div>
+        </Card>
+
+        <Card className="w-full flex items-start" shadow="none">
           <div className="w-60 h-60 rounded-md bg-slate-400" />
           <Text align="right" fontSize="sm" color="GrayText">
-            안녕
+            확인함
           </Text>
         </Card>
+
+        <Card className="w-full flex items-start" shadow="none">
+          <div className="w-60 h-60 rounded-md bg-slate-400" />
+          <Text align="right" fontSize="sm" color="GrayText">
+            확인함
+          </Text>
+        </Card>
+      </div>
+
+      <div className="bg-white w-full max-w-[768px] flex items-center fixed left-1/2 -translate-x-1/2 h-16 bottom-20 py-4 px-4">
+        <InputGroup size="lg" className="w-full">
+          <Input placeContent={"메시지를 입력하세요"}></Input>
+          <InputRightAddon children="전송" cursor={"pointer"} />
+        </InputGroup>
+
+        <BiMicrophone className="mx-4" size={22} />
       </div>
     </>
   );
