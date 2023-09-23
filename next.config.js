4886853d96e923c368b263c816891c30b853d377
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-09-24 01:28:14 +0900

    Fix : conflict

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
+<<<<<<< HEAD
     domains: ["source.unsplash.com", "picsum.photos", "kr.object.ncloudstorage.com", "encrypted-tbn2.gstatic.com"]
+=======
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**"
+      }, {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+>>>>>>> 9487e35 (chore : nextConfig image domain 모든 도메인 오픈)
   }
 };
 
