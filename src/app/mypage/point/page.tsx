32734fef3079a87dbc5dab531508030461a17583
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-09-24 07:04:08 +0900

    Feat : UI 업데이트 3

--- src/app/mypage/point/page.tsx
+++ src/app/mypage/point/page.tsx
@@ -4,7 +4,7 @@ import ProfileHeader from "@/components/mypage/ProfileHeader";
 const Page = () => {
   return (
     <>
-      <ProfileHeader text={"UserName"} />
+      <ProfileHeader text={"내 포인트 사용하기"} />
       <ProfileTabs />
     </>
   );
