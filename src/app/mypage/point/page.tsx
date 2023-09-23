acb9a5c35d2c44fba9e2c9948a062ee6b807db40
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-09-24 07:26:53 +0900

    Feat : ui update

--- src/app/mypage/point/page.tsx
+++ src/app/mypage/point/page.tsx
@@ -6,6 +6,11 @@ const Page = () => {
     <>
       <ProfileHeader text={"내 포인트 사용하기"} />
       <ProfileTabs />
+      <img
+        src="https://cdn.discordapp.com/attachments/1152124462322421816/1155268042947838024/270135425-83802ea0-aea9-4838-b5b2-92a3d89e6240.png"
+        alt=""
+      />
+      <img src="https://kr.object.ncloudstorage.com/kikids/111111.png" alt="" />
     </>
   );
 };
