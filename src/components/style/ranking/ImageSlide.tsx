8c799e490ccd8b5551db6cbfab8d4ddc4910e4e7
Dusuna <94776135+dusunax@users.noreply.github.com>
2023-09-24 06:24:40 +0900

    Feat : UI-2

--- src/components/style/ranking/ImageSlide.tsx
+++ src/components/style/ranking/ImageSlide.tsx
@@ -3,14 +3,18 @@ import Image from "next/image";
 
 const ImageSlide = ({ feedsList }: { feedsList: Feed[] }) => {
   const pictures = feedsList[0].pictures.slice(0, 3).concat(Array(3 - feedsList[0].pictures.length).fill("/images"));
+  console.log(pictures);
 
   return (
     <div className="flex gap-2 px-3">
-      {pictures.map((picture, index) => (
-        <div key={index} className="relative bg-black flex-1 h-52 rounded-lg overflow-hidden">
-          <Image fill={true} objectFit="cover" src={picture} alt={"image"} />
-        </div>
-      ))}
+      {pictures.map((picture, index) => {
+        console.log(picture);
+        return (
+          <div key={index} className="relative bg-black flex-1 h-52 rounded-lg overflow-hidden">
+            <Image fill={true} objectFit="cover" src={picture} alt={"image"} />
+          </div>
+        );
+      })}
     </div>
   );
 };
