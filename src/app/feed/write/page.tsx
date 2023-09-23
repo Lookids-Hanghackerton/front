d9df2869e85f4b95ddf4829131045d5109f8c58f
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-09-23 21:01:26 +0900

    Feat : 게시글 작성 - 상품태그 검색 및 검색결과 추가

--- src/app/feed/write/page.tsx
+++ src/app/feed/write/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ReactNode, useState } from "react";
+import React, { useState, ChangeEvent, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
@@ -8,17 +8,52 @@ import Tags from "@/components/style/Tags";
 import Content from "@/components/feed/Content";
 import ImageUpload from "@/components/feed/ImageUpload";
 
+interface ProductInfo {
+  productsId: number;
+  productsName: string;
+  manufacturesName: string;
+  productsImage: string[];
+}
+
 const Page = () => {
   const router = useRouter();
   const pathname = usePathname();
 
-  const [productInfo, setProductInfo] = useState({
-    productName: "",
-    company: "",
-  });
+  const [searchProduct, setSearchProduct] = useState("");
+  const [productInfo, setProductInfo] = useState<
+    { productsId: number; manufacturesName: string; productsName: string }[]
+  >([]);
   const [isOpenModal, setIsOpenModal] = useState(false);
 
   const openProductTagModal = () => setIsOpenModal(!isOpenModal);
+
+  const handleProductChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    const inputValue = e.target.value;
+    setSearchProduct(inputValue);
+
+    try {
+      const response = await fetch(`http://125.181.213.112:8080/product/find/${inputValue}`);
+      const data = await response.json();
+      console.log(data.data.productsList);
+      setProductInfo(data.data.productsList);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
+  // useEffect(() => {
+  //   fetch(`http://125.181.213.112:8080/feed/write`)
+  //     .then(resp => resp.json())
+  //     .then(result => {
+  //       setProductInfo(result);
+  //     });
+  // }, []);
+
+  const handleClickProduct = () => {
+    setIsOpenModal(!isOpenModal);
+    setSearchProduct("");
+  };
 
   return (
     <>
@@ -27,22 +62,40 @@ const Page = () => {
         className="text-gray-500 flex justify-center items-center cursor-pointer bg-red-300"
         onClick={openProductTagModal}
       >
-        {"상품 태그를 추가해 보세요"}
+        {"#상품 태그를 추가해 보세요"}
       </button>
       {isOpenModal && (
-        <div className=" bg-black opacity-25 fixed top-0 left-0 w-full h-full flex justify-center items-center">
-          <div className="w-6/12 h-1/4 bg-pink-500 ">
-            <input
-              type="text"
-              className="min-w-0 w-10/12 mx-8 my-8 p-4 font-medium text-sm outline-0 bg-gray-100 rounded-lg "
-              placeholder="상품정보를 입력하세요"
-            />
-            <button
-              className="min-w-0 w-2/12 mx-8 my-8 p-4 font-medium text-sm outline-0 bg-blue-500 rounded-lg"
-              onClick={() => setIsOpenModal(!isOpenModal)}
-            >
-              {"완료 "}
-            </button>
+        <div>
+          <div className=" bg-black  opacity-25 fixed top-0 left-0 w-full h-full flex justify-center items-center">
+            <div className="w-6/12 h-1/4 bg-white ">
+              <input
+                type="text"
+                value={searchProduct}
+                className="min-w-0 w-10/12 mx-8 my-8 p-4 font-medium text-sm outline-0 bg-gray-100 rounded-lg "
+                placeholder="상품정보를 입력하세요"
+                onChange={handleProductChange}
+              />
+              <ul>
+                {productInfo &&
+                  productInfo.map(item => {
+                    console.log(item);
+                    return (
+                      <li key={item.productsId}>
+                        상품명/회사 : {item.productsName} / {item.manufacturesName}
+                      </li>
+                    );
+                  })}
+              </ul>
+
+              <div className="flex justify-center items-center">
+                <button
+                  className="min-w-0 w-2/12 mx-8 my-8 p-4 font-medium text-sm outline-0 bg-blue-500 rounded-lg"
+                  onClick={handleClickProduct}
+                >
+                  {"완료 "}
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       )}
