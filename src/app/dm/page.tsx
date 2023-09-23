2e2cea7384d07396bdf70afc3a79611167e9dbca
csyun <changsoonyun@naver.com>
2023-09-24 06:51:57 +0900

    Refactor : DM Image부분 더미데이터로 교체

--- src/app/dm/page.tsx
+++ src/app/dm/page.tsx
@@ -1,9 +1,18 @@
 import DM from "@/components/dm/DM";
+import { Cookie } from "@/interfaces/Cookie";
+import { cookies } from "next/headers";
+import { getTrendFeeds } from "@/apis/controllers/style/useGetTrendFeeds";
 
-const Page = () => {
+const Page = async () => {
+  const cookieStore = cookies();
+  const cookie = cookieStore.getAll().map(cookie => JSON.parse(cookie.value))[0] as Cookie;
+
+  console.log(cookie);
+  const feedList = await getTrendFeeds("인기순", 0);
+
   return (
     <div>
-      <DM />
+      <DM feedList={feedList} profileImage={cookie.profileImage} memberNickName={cookie.memberNickName} />
     </div>
   );
 };
--- src/components/dm/DM.tsx
+++ src/components/dm/DM.tsx
@@ -1,37 +1,57 @@
+"use client";
+
 import { Button, Card, Input, InputGroup, InputRightAddon, Text } from "@chakra-ui/react";
 import { BiMicrophone } from "react-icons/bi";
+import Image from "next/image";
+import { useGetTrendFeeds } from "@/apis/controllers/useGetTrendFeeds";
+import { FeedResponse } from "@/apis/interfaces/Feeds";
 
-const DM = () => {
+const DM = ({
+  profileImage,
+  memberNickName,
+  feedList,
+}: {
+  profileImage: string;
+  memberNickName: string;
+  feedList: FeedResponse;
+}) => {
   const date = new Date();
   const dateString = `${date.getMonth() + 1}월 ${date.getDate()}일 ${date.getHours()}:${date.getMinutes()}`;
+  const data = useGetTrendFeeds({ filtered: "인기순", feedList: feedList });
 
   return (
     <>
       <div className="flex justify-center flex-col items-center mb-10">
-        <div className="w-40 h-40 rounded-full bg-slate-400 mb-2" />
-        <Text fontSize={"xl"}>사용자명</Text>
-        <Text fontSize={"md"}>소개글</Text>
+        <div className="relative w-40 h-40 rounded-full bg-slate-400 mb-2 overflow-hidden">
+          <Image fill={true} objectFit={"cover"} src={profileImage} alt={"avatar"} />
+        </div>
+        <Text fontSize={"xl"}>{memberNickName}</Text>
+        <Text fontSize={"md"}>반가워요!</Text>
 
         <Button className="mt-4">프로필 보기</Button>
       </div>
-      <div className="flex justify-center flex-col gap-4 items-center mb-10 px-4">
+      <div className="flex justify-center flex-col gap-4 items-center mb-16 px-4">
         <Text fontSize={"md"} color={"GrayText"}>
           {dateString}
         </Text>
 
         <Card className="w-full flex items-end" shadow="none">
-          <div className="w-60 h-40 rounded-md bg-slate-400" />
+          <div className="relative w-60 h-40 rounded-md bg-slate-400 overflow-hidden">
+            <Image fill={true} objectFit={"cover"} src={data.data.feedsList[0].pictures[0]} alt={"image"} />
+          </div>
         </Card>
 
         <Card className="w-full flex items-end" shadow="none">
-          <div className="w-60 h-60 rounded-md bg-slate-400" />
+          <div className="relative w-60 h-60 rounded-md bg-slate-400 overflow-hidden">
+            <Image fill={true} objectFit={"cover"} src={data.data.feedsList[1].pictures[0]} alt={"image"} />
+          </div>
           <Text align="right" fontSize="sm" color="GrayText">
             확인함
           </Text>
         </Card>
 
-        <Card className="w-full flex items-end" shadow="none">
-          <div className="w-60 px-6 py-4 rounded-md bg-slate-200">
+        <Card className=" w-full flex items-end" shadow="none">
+          <div className="relative w-60 px-6 py-4 rounded-md bg-slate-200">
             <Text fontSize="sm" color="GrayText">
               안녕하세요
             </Text>
@@ -47,14 +67,18 @@ const DM = () => {
         </Card>
 
         <Card className="w-full flex items-start" shadow="none">
-          <div className="w-60 h-60 rounded-md bg-slate-400" />
+          <div className="relative w-60 h-60 rounded-md bg-slate-400 overflow-hidden">
+            <Image fill={true} objectFit={"cover"} src={data.data.feedsList[2].pictures[0]} alt={"image"} />
+          </div>
           <Text align="right" fontSize="sm" color="GrayText">
             확인함
           </Text>
         </Card>
 
         <Card className="w-full flex items-start" shadow="none">
-          <div className="w-60 h-60 rounded-md bg-slate-400" />
+          <div className="relative w-60 h-60 rounded-md bg-slate-400 overflow-hidden">
+            <Image fill={true} objectFit={"cover"} src={data.data.feedsList[3].pictures[0]} alt={"image"} />
+          </div>
           <Text align="right" fontSize="sm" color="GrayText">
             확인함
           </Text>
